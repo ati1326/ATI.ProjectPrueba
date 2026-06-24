9aeec41c7a917392af380396c8c6419a5b050b55
Desarrollo2 <desarrollo2@ati.net.co>
2026-06-24 09:13:43 -0500

    Primer controller con los https Get[id] Get, Post, Put, Delete

--- ATI.ProjectPrueba.Backend/Program.cs
+++ ATI.ProjectPrueba.Backend/Program.cs
@@ -2,24 +2,24 @@ using ATI.ProjectPrueba.Backend.Data;
 using Microsoft.EntityFrameworkCore;
 
 
-var 
-    
-    builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection"));
 
-  
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseCors(x => x
+ .AllowAnyMethod()
+ .AllowAnyHeader()
+ .SetIsOriginAllowed(origin => true)
+ .AllowCredentials());
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
