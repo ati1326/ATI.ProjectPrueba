0563db9cccf2034fb77f9d2211a9717ea80009a8
Desarrollo2 <desarrollo2@ati.net.co>
2026-06-25 11:31:20 -0500

    diseño de pagina

---
+++ ATI.ProjectPrueba.Frontend/Shared/GenericList.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ATI.ProjectPrueba.Frontend.Shared
+{
+    public partial class GenericList<TItem>
+    {
+            [Parameter] public RenderFragment? Loading { get; set; }
+            [Parameter] public RenderFragment? NoRecords { get; set; }
+            [EditorRequired, Parameter] public RenderFragment? Body { get; set; } = null!;
+            [EditorRequired, Parameter] public List<TItem>? MyList { get; set; } = null!;
+    }
+}
+
+
