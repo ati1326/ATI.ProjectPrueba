bed1546159831086618b830154f3ddc3fc2eea71
Desarrollo2 <desarrollo2@ati.net.co>
2026-06-25 07:53:38 -0500

    se muestra una tabla basica

--- ATI.ProjectPrueba.Frontend/Program.cs
+++ ATI.ProjectPrueba.Frontend/Program.cs
@@ -7,7 +7,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7181/weather") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7050/") });
 builder.Services.AddScoped<IRepository, Repository>();
 
 await builder.Build().RunAsync();
